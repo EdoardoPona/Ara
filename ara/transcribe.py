43d3e48789321954f51dd517a77287e044f453fe
Edoardo Pona <edoardo.pona@gmail.com>
2023-01-06 21:00:07 +0000

    cache dir pyannote

--- ara/transcribe.py
+++ ara/transcribe.py
@@ -3,37 +3,27 @@ import whisper
 from pyannote.audio import Pipeline
 import os 
 
-def transcribe(file_name: str, verbose=False, language=None, model_path=None):
+def transcribe(
+        file_name: str, 
+        verbose=False, 
+        language=None, 
+        transcript_model_path=None, 
+        pyannote_cache_dir=None
+    ):
     print('Transcribing')
-    if model_path is None:
+    if transcript_model_path is None:
         model = whisper.load_model("base")
     else:
-        model = whisper.load_model(model_path)
+        model = whisper.load_model(transcript_model_path)
 
-#
-#    # load audio and pad/trim it to fit 30 seconds
-#    audio = whisper.load_audio(file_name)
-#    audio = whisper.pad_or_trim(audio)
-#
-#    # make log-Mel spectrogram and move to the same device as the model
-#    mel = whisper.log_mel_spectrogram(audio).to(model.device)
-#
-#    if language is None:
-#        _, probs = model.detect_language(mel)
-#        langauge = max(probs, key=probs.get)
-#        print(f"Detected language: {language}")
-#
-#    # decode the audio
-#    options = whisper.DecodingOptions(language=language)
-#    result = whisper.decode(model, mel, options).text
-#
     result = model.transcribe(file_name, verbose=verbose, language=language)
     segments = [{'start': r['start'], 'end': r['end'], 'text': r['text']} for r in result['segments']]
 
     print('Diarizing') 
     pipeline = Pipeline.from_pretrained(
         "pyannote/speaker-diarization",
-	    use_auth_token=os.getenv('HUGGINGFACE_TOKEN')
+	    use_auth_token=os.getenv('HUGGINGFACE_TOKEN'),
+        cache_dir=pyannote_cache_dir
     )
 
     # NOTE diarization does not currently work in languages other than English 
